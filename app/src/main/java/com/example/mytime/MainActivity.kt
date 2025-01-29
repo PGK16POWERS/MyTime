95e8acbde93898b3933f5cd373db1a1e64aa9b8f
programmer Kartier <progrmmerKartier@gmail.com>
2025-01-30 01:49:21 +0200

    Minor Touches

--- app/src/main/java/com/example/mytime/MainActivity.kt
+++ app/src/main/java/com/example/mytime/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mytime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
