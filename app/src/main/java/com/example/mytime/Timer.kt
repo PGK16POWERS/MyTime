1ab3e072a6c45f59b4a755e055ce6ec45d0201e2
programmer Kartier <progrmmerKartier@gmail.com>
2025-01-30 01:16:55 +0200

    Skeletal Touches

--- app/src/main/java/com/example/mytime/Timer.kt
+++ app/src/main/java/com/example/mytime/Timer.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Globe : Fragment() {
+class Timer : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_globe, container, false)
+        return inflater.inflate(R.layout.fragment_timer, container, false)
     }
 
 }
